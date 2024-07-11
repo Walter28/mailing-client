b9ce5ac01580ea610c33184b52e59c3d298adcd2
Walter28 <walterchristian28@gmail.com>
2024-07-12 00:50:55 +0200

    creating new code for security

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 import smtplib
+import os
 
-server = smtplib.SMTP('smtp.gmail.com', 25)
+# Récupérer le mot de passe à partir d'une variable d'environnement
+password = os.getenv('EMAIL_PASSWORD')
+
+# Créer une connexion sécurisée au serveur SMTP de Gmail
+server = smtplib.SMTP('smtp.gmail.com', 587)
+server.starttls()  # Commencer TLS pour sécuriser la connexion
+server.ehlo()  # S'identifier auprès du serveur
+
+# Authentification
+try:
+    server.login('walterchristian28@gmail.com', password)
+    print("Authentification réussie")
+except smtplib.SMTPAuthenticationError:
+    print("Erreur d'authentification")
+except Exception as e:
+    print(f"Erreur : {e}")
+finally:
+    server.quit()
